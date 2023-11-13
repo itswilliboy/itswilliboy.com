509a013bbfb97246cb9c11dbdb096b15d62cf69d
ItsWilliboy <83978878+itswilliboy@users.noreply.github.com>
2023-11-13 23:55:56 +0100

    Add error text

--- src/app/components/TikTokForm.tsx
+++ src/app/components/TikTokForm.tsx
@@ -4,14 +4,14 @@ import { FormEvent } from "react";
 import getVideo from "../utils/getVideo";
 import { useRouter } from "next/navigation";
 
-export default function TikTokForm({loadingSetter}: {loadingSetter: (value: boolean) => void}) {
+export default function TikTokForm({loadingSetter, errorSetter}: {loadingSetter: (value: boolean) => void, errorSetter: (value: string | null) => void}) {
     const router = useRouter()
     const onSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        loadingSetter(true)
-
         const data = new FormData(e.currentTarget)
         const url = data.get("url")?.toString()
+        if (!url) { errorSetter("You need to enter a URL."); return }
+        loadingSetter(true)
 
         getVideo(url!).then(
             (e) => {
@@ -25,7 +25,7 @@ export default function TikTokForm({loadingSetter}: {loadingSetter: (value: bool
     return (
         <>
             <form onSubmit={(e) => { onSubmit(e) }} className="flex flex-col gap-4 items-center">
-                <input onClick={(e) => { e.currentTarget.select() }} type="text" name="url" placeholder="Enter a TikTok Video URL" className="p-2 rounded-lg outline-none bg-transparent w-[325px] md:w-[600px] text-center border-2 focus:border-violet-400 hover:border-violet-400 transition-colors" autoComplete="off"/>
+                <input onClick={(e) => { e.currentTarget.select() }} onChange={() => { errorSetter(null) }} type="text" name="url" placeholder="Enter a TikTok Video URL" className="p-2 rounded-lg outline-none bg-transparent w-[325px] md:w-[600px] text-center border-2 focus:border-violet-400 hover:border-violet-400 transition-colors" autoComplete="off"/>
                 <input type="submit" value="Download" className="text-white p-2 cursor-pointer border-2 w-48 hover:border-y- rounded-lg hover:bg-violet-400/40 transition-colors" />
             </form>
         </>
--- src/app/tiktok/page.tsx
+++ src/app/tiktok/page.tsx
@@ -6,6 +6,7 @@ import TikTokForm from "../components/TikTokForm"
 
 export default function TikTok() {
     let [isLoading, setIsLoading] = useState<boolean>(false)
+    let [error, setError] = useState<string | null>(null)
 
     return (
         <>
@@ -14,8 +15,11 @@ export default function TikTok() {
                 {isLoading && (
                     <h1 className="text-white absolute transform -translate-y-24 text-3xl">Loading...</h1>
                 )}
+                {error && (
+                    <p className="text-red-500 absolute transform -translate-y-16 text-md">{error}</p>
+                )}
                 <div className="pt-5">
-                    <TikTokForm loadingSetter={setIsLoading}/>
+                    <TikTokForm loadingSetter={setIsLoading} errorSetter={setError}/>
                 </div>
             </div>
         </div>
