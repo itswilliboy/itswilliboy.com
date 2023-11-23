13512ee24f092955d94946e5676f175856f355bf
ducki <qwuakers@gmail.com>
2023-11-23 23:55:31 +0400

    Fixed clock taking an initial second to load (#3)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,23 @@
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 
+const updateTime = (): { clock: string, date: string } => {
+  const [clock, date] = new Date()
+    .toLocaleString('en-GB', { hour12: false })
+    .split(',')
+
+  return {
+    clock: clock.trim(),
+    date: date.trim()
+  }
+}
+
 export default function Home (): JSX.Element {
-  const [time, setTime] = useState<{ clock: string, date: string }>()
+  const [time, setTime] = useState(updateTime())
 
   useEffect(() => {
     setInterval(() => {
-      const [clock, date] = new Date()
-        .toLocaleString('en-GB', { hour12: false })
-        .split(',')
-      setTime({ clock, date })
+      setTime(updateTime())
     }, 1000)
   }, [])
 
@@ -29,7 +37,7 @@ export default function Home (): JSX.Element {
           className="flex justify-center mt-52 text-center"
           style={{ textShadow: '5px 5px black' }}
         >
-          <h1 className="text-6xl font-bold">
+          <h1 className="text-6xl font-bold" suppressHydrationWarning>
             {time?.clock}
             <br />
             {time?.date}
