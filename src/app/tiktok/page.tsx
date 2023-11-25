4061fef694c95aa33006333a2b49e1796fe2daf4
ItsWilliboy <83978878+itswilliboy@users.noreply.github.com>
2023-11-25 13:42:28 +0100

    Add "Download Video" button

--- src/app/tiktok/page.tsx
+++ src/app/tiktok/page.tsx
@@ -44,21 +44,28 @@ export default function TikTok (): JSX.Element {
               />
               Your browser does not support videos.
             </video>
-
+            <a
+              className="p-2 bg-slate-700 rounded-lg hover:bg-slate-600 transition-colors"
+              href={URL.createObjectURL(video.video as Blob)}
+              download={`${video.metadata.aweme_id}.mp4`}
+            >
+              Download Video
+            </a>
             <div className="max-w-screen-sm mb-10 px-3">
               <p className="font-semibold text-2xl">
                 {video.metadata.creator.nickname}
                 &nbsp;
-                <a className="text-base text-gray-500" href={`https://www.tiktok.com/@${video.metadata.creator.unique_id}`}>
+                <a
+                  className="text-base text-gray-500"
+                  href={`https://www.tiktok.com/@${video.metadata.creator.unique_id}`}
+                >
                   (@{video.metadata.creator.unique_id})
                 </a>
               </p>
 
-              <div className='h-0.5 w-full bg-gray-700 mb-2' />
+              <div className="h-0.5 w-full bg-gray-700 mb-2" />
 
-              <p>
-                {video.metadata.description}
-              </p>
+              <p>{video.metadata.description}</p>
             </div>
           </div>
         )}
